b12c081503b326d93c10bcba359dbebaa593907a
Danil Kaist <kaystd@gmail.com>
2019-08-19 17:48:49 +0700

    Add string encryption

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,4 +1,18 @@
-import { curryTwoFlip, parseHexStringToBuffer, pipe } from '../utils'
+import {
+  base64ToBuffer,
+  base64ToUtf8,
+  bufferToBase64,
+  complementMessage,
+  curryTwoFlip,
+  encryptString,
+  joinMessage,
+  parseHexStringToBuffer,
+  pipe,
+  splitMessage,
+  truncateMessage,
+  utf8ToBase64,
+  utf8ToBuffer,
+} from '../utils'
 
 describe('Utils functions', () => {
 
@@ -56,6 +70,116 @@ describe('Utils functions', () => {
       const mul = (a: number): (b: number) => number => (b: number): number => a * b
       const piped = pipe(add, mul(3))(2, 3)
       expect(piped).toEqual(15)
+    })
+  })
+
+  describe('utf8ToBuffer', () => {
+    it('10 symbols', () => {
+      const utf8 = 'abcdefg123'
+      const buffer = [97, 98, 99, 100, 101, 102, 103, 49, 50, 51]
+      expect(utf8ToBuffer(utf8)).toEqual(buffer)
+    })
+  })
+
+  describe('base64ToBuffer', () => {
+    it('10 symbols', () => {
+      const base64 = 'YWJjZGVmZ2gxMjM='
+      const buffer = [97, 98, 99, 100, 101, 102, 103, 104, 49, 50, 51]
+      expect(base64ToBuffer(base64)).toEqual(buffer)
+    })
+  })
+
+  describe('bufferToBase64', () => {
+    it('10 symbols', () => {
+      const buffer = [97, 98, 99, 100, 101, 102, 103, 104, 49, 50, 51]
+      const base64 = 'YWJjZGVmZ2gxMjM='
+      expect(bufferToBase64(buffer)).toEqual(base64)
+    })
+  })
+
+  describe('complementMessage', () => {
+    it('9 octets', () => {
+      const originalMessage = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      const complementedMessage = [1, 2, 3, 4, 5, 6, 7, 8, 9, 128, 0, 0, 0, 0, 0, 0]
+      expect(complementMessage(originalMessage)).toEqual(complementedMessage)
+    })
+    it('16 octets', () => {
+      const originalMessage = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+      const complementedMessage = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      ]
+      expect(complementMessage(originalMessage)).toEqual(complementedMessage)
+    })
+  })
+
+  describe('invComplementMessage', () => {
+    it('9 octets', () => {
+      const complementedMessage = [1, 2, 3, 4, 5, 6, 7, 8, 9, 128, 0, 0, 0, 0, 0, 0]
+      const originalMessage = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      expect(truncateMessage(complementedMessage)).toEqual(originalMessage)
+    })
+    it('16 octets', () => {
+      const complementedMessage = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      ]
+      const originalMessage = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+      expect(truncateMessage(complementedMessage)).toEqual(originalMessage)
+    })
+  })
+
+  describe('utf8ToBase64', () => {
+    it('string with utf-8 symbols', () => {
+      const utf8String = 'Text with emoji 😀😃😄'
+      const base64String = 'VGV4dCB3aXRoIGVtb2ppIPCfmIDwn5iD8J+YhA=='
+      expect(utf8ToBase64(utf8String)).toEqual(base64String)
+    })
+  })
+
+  describe('base64ToUtf8', () => {
+    it('string with utf-8 symbols', () => {
+      const base64String = 'VGV4dCB3aXRoIGVtb2ppIPCfmIDwn5iD8J+YhA=='
+      const utf8String = 'Text with emoji 😀😃😄'
+      expect(base64ToUtf8(base64String)).toEqual(utf8String)
+    })
+  })
+
+  describe('splitMessage', () => {
+    it('48 octets', () => {
+      const message = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48
+      ]
+      const chunks = [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+        [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32],
+        [33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48],
+      ]
+      expect(splitMessage(message)).toEqual(chunks)
+    })
+  })
+
+  describe('joinMessage', () => {
+    it('48 octets', () => {
+      const chunks = [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+        [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32],
+        [33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48],
+      ]
+      const message = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48
+      ]
+      expect(joinMessage(chunks)).toEqual(message)
+    })
+  })
+
+  describe('encryptString', () => {
+    it('string with utf-8 symbols', () => {
+      const key = 'YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXoxMjM0NTY='
+      const message = 'Hello World! 😀😃😄'
+      const encrypted = '0E2wSxOZUrnFOeWGoBeFX8Nhj0Dw7T2eL4+4M89eLfI='
+
+      expect(encryptString(message, key)).toEqual(encrypted)
     })
   })
 })
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { decrypt, encrypt } from './grasshopper'
+
 /**
  * Curries a function with two arguments and flip them
  * @param fn
@@ -11,7 +13,7 @@ export const curryTwoFlip = <A, B, R>(fn: (a: A, b: B) => R): ((b: B) => (a: A) 
  * @param fn1
  * @param fns
  */
-export const pipe = <A extends any[], R>(fn1: (...args: A) => R, ...fns: Array<(a: R) => R>): ((...args: A) => R) => {
+export const pipe = <A extends any[], R extends any>(fn1: (...args: A) => R, ...fns: Array<(a: R) => R>): ((...args: A) => R) => {
   const piped = fns.reduce(
     (prevFn, nextFn) => (value: R): R => nextFn(prevFn(value)),
     value => value
@@ -27,4 +29,95 @@ export const parseHexStringToBuffer = (hexString: string): number[] => {
   const list: string[] | null = hexString.match(/.{1,2}/g)
 
   return list ? list.map(s => Number(`0x${s}`)) : []
+}
+
+/**
+ * Converts Utf-8 string to array of 8-bit unsigned integer values
+ * @param utf8String
+ */
+export const utf8ToBuffer = (utf8String: string): number[] => {
+  const textEncoder = new TextEncoder()
+  const buffer = textEncoder.encode(utf8String)
+
+  return Array.from(buffer)
+}
+
+/**
+ * Complements message to block's length
+ * @param buffer
+ */
+export const complementMessage = (buffer: number[]): number[] => {
+  const mapping = (_: number, ind: number): number => ind === 0 ? 128 : 0
+  const lengthModulo = buffer.length % 16
+  return lengthModulo === 0
+    ? buffer.concat(Array(16).fill(0).map(mapping))
+    : buffer.concat(Array(16 - lengthModulo).fill(0).map(mapping))
+}
+
+/**
+ * Truncates padded bits
+ * @param buffer
+ */
+export const truncateMessage = (buffer: number[]): number[] => {
+  const index = buffer.lastIndexOf(128)
+
+  return buffer.slice(0, index)
+}
+
+/**
+ * Converts Utf-8 string to Base-64 string
+ * @param utf8String
+ */
+export const utf8ToBase64 = (utf8String: string): string => Buffer.from(utf8String).toString('base64')
+
+/**
+ * Converts Base-64 string to Utf-8 string
+ * @param base64String
+ */
+export const base64ToUtf8 = (base64String: string): string => Buffer.from(base64String, 'base64').toString()
+
+/**
+ * Converts array of 8-bit unsigned integer values to Base-64 string
+ * @param buffer
+ */
+export const bufferToBase64 = (buffer: number[]): string => Buffer.from(buffer).toString('base64')
+
+/**
+ * Converts Base-64 string to array of 8-bit unsigned integer values
+ * @param base64String
+ */
+export const base64ToBuffer = (base64String: string): number[] => Array.from(Buffer.from(base64String, 'base64'))
+
+/**
+ * Split message to 128-bit chunks
+ * @param buffer
+ */
+export const splitMessage = (buffer: number[]): number[][] => {
+  const chunksNumber = Math.floor(buffer.length / 16)
+
+  return Array(chunksNumber).fill(0).map((val, ind) => buffer.slice(ind * 16, (ind + 1) * 16))
+}
+
+/**
+ * Joins 128-bit chunks into message
+ * @param buffers
+ */
+export const joinMessage = (buffers: number[][]): number[] => buffers.flat()
+
+/**
+ * Encrypts Utf-8 string message and returns encrypted message in Base-64
+ * @param message Utf-8 string
+ * @param masterKey Base-64 string
+ *
+ */
+export const encryptString = (message: string, masterKey: string): string => {
+  const masterKeyBuffer = base64ToBuffer(masterKey)
+  const curriedEncrypt = curryTwoFlip(encrypt)(masterKeyBuffer)
+
+  const complemented = pipe(utf8ToBuffer, complementMessage)(message)
+  const split = splitMessage(complemented)
+  const encrypted = split.map(curriedEncrypt)
+  const joined = joinMessage(encrypted)
+
+  return bufferToBase64(joined)
 }
